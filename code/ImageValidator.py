49d7102167585745d213d5a9210442b2332728ab
Yair_Davidoff <88335811+yairda2@users.noreply.github.com>
2024-01-14 00:55:16 +0200

    update

--- code/ImageValidator.py
+++ code/ImageValidator.py
@@ -1,3 +1,5 @@
+import subprocess
+import sys
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
@@ -5,8 +7,12 @@ import os
 import pandas as pd
 import shutil
 from datetime import datetime
-current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
 class ImageValidator:
+    """
+    The main class for the Image Validator application
+    """
     def __init__(self, root):
         self.root = root
         self.root.title("Image Validator")
@@ -15,30 +21,38 @@ class ImageValidator:
         self.image_path = ""
         self.valid = tk.IntVar()
         self.explanations = []
+        self.reason_description_map = {}
 
         self.create_widgets()
 
     def create_widgets(self):
-        select_image_btn = tk.Button(self.root, text="Select Image", command=self.select_image)
+        """
+        Creates the widgets for the GUI
+        :return:
+        """
+        select_image_btn = tk.Button(self.root, text="Select Image File", command=self.select_image)
         select_image_btn.grid(row=0, column=0, columnspan=3, pady=10)
 
+        select_dir_btn = tk.Button(self.root, text="Select Directory", command=self.select_directory)
+        select_dir_btn.grid(row=1, column=0, columnspan=3, pady=10)
+
         valid_label = tk.Label(self.root, text="Is the person valid?")
-        valid_label.grid(row=1, column=0, columnspan=3)
+        valid_label.grid(row=2, column=0, columnspan=3)
 
         valid_yes = tk.Radiobutton(self.root, text="Valid", variable=self.valid, value=1)
-        valid_yes.grid(row=2, column=0, padx=5)
+        valid_yes.grid(row=3, column=0, padx=5)
         valid_no = tk.Radiobutton(self.root, text="Not Valid", variable=self.valid, value=0)
-        valid_no.grid(row=2, column=1, padx=5)
+        valid_no.grid(row=3, column=1, padx=5)
 
         explanation_label = tk.Label(self.root, text="Reason & Description (if not valid):")
-        explanation_label.grid(row=3, column=0, columnspan=3)
+        explanation_label.grid(row=4, column=0, columnspan=3)
 
         line_number_label = tk.Label(self.root, text="Line Number")
-        line_number_label.grid(row=4, column=0)
+        line_number_label.grid(row=5, column=0)
         reason_label = tk.Label(self.root, text="Reason")
-        reason_label.grid(row=4, column=1)
+        reason_label.grid(row=5, column=1)
         description_label = tk.Label(self.root, text="Description")
-        description_label.grid(row=4, column=2)
+        description_label.grid(row=5, column=2)
 
         self.line_number_entries = []
         self.reason_entries = []
@@ -48,70 +62,168 @@ class ImageValidator:
             line_number_entry.insert(tk.END, str(i + 1))
             reason_entry = tk.Entry(self.root)
             description_entry = tk.Entry(self.root)
-            line_number_entry.grid(row=i + 5, column=0, padx=5)
-            reason_entry.grid(row=i + 5, column=1, padx=5)
-            description_entry.grid(row=i + 5, column=2, padx=5)
+            line_number_entry.grid(row=i + 6, column=0, padx=5)
+            reason_entry.grid(row=i + 6, column=1, padx=5)
+            description_entry.grid(row=i + 6, column=2, padx=5)
             self.line_number_entries.append(line_number_entry)
             self.reason_entries.append(reason_entry)
             self.description_entries.append(description_entry)
 
         save_btn = tk.Button(self.root, text="Save", command=self.save_data)
-        save_btn.grid(row=20, column=0, columnspan=3, pady=10)
+        save_btn.grid(row=21, column=0, columnspan=3, pady=10)
 
     def select_image(self):
+        """
+        Selects the image to be validated
+        :return:
+        """
         self.image_path = filedialog.askopenfilename()
         if self.image_path:
-            self.clear_entries_except_line_numbers()
-            image = Image.open(self.image_path)
-            image.thumbnail((300, 300))
-            photo = ImageTk.PhotoImage(image)
-            img_label = tk.Label(self.root, image=photo)
-            img_label.image = photo
-            img_label.grid(row=0, column=3, rowspan=18, padx=10)
+            if os.path.isdir(self.image_path):
+                # next to the button dir, show the selected dir path, not only massage box
+                messagebox.showinfo("Directory Selected", f"You are working on directory: {self.image_path}")
+                # add the code to show the selected dir name
+                self.clear_entries_except_line_numbers()
+
+
+            else:
+                # next to the button, show the selected image
+                messagebox.showinfo("Image Selected", f"You are working on image: {self.image_path}")
+                # if self.image_path.endswith((".jpg", ".jpeg", ".png")):
+                image = Image.open(self.image_path)
+                image.thumbnail((300, 300))
+                photo = ImageTk.PhotoImage(image)
+                img_label = tk.Label(self.root, image=photo)
+                img_label.image = photo
+                img_label.grid(row=0, column=3, rowspan=18, padx=10)
+
+    def select_directory(self):
+        """
+        Selects the directory containing the images
+        :return:
+        """
+        dir_path = filedialog.askdirectory()
+        if dir_path:
+            self.image_path = dir_path
+            messagebox.showinfo("Directory Selected", f"You are working on directory: {self.image_path}")
+            self.clear_image()  # Clear the displayed image
 
     def save_data(self):
+        """
+        Saves the data to Excel and copies the image to the 'photos' folder
+        :return:
+        """
         if not self.image_path:
-            messagebox.showerror("Error", "Please select an image.")
+            messagebox.showerror("Error", "Please select an image or directory.")
             return
 
-        image_name = os.path.basename(self.image_path)
-        destination = os.path.join(f'{current_dir}/photos', image_name)
-        os.makedirs("photos", exist_ok=True)
-        Image.open(self.image_path).save(destination)
+        # Get the value of the valid/invalid radio button
+        valid_value = self.valid.get()
 
-        data = {
-            "Image Path": [image_name],
-            "Validity": ["Valid" if self.valid.get() == 1 else "Not Valid"]
-        }
+        if os.path.isdir(self.image_path):
+            # Directory processing logic (added in the previous code segment)
+            processed_images = []
+            for filename in os.listdir(self.image_path):
+                if filename.endswith((".jpg", ".jpeg", ".png")):
+                    image_name = os.path.basename(filename)
+                    if self.image_not_in_excel(image_name):
+                        destination = os.path.join("photos", image_name)
+                        os.makedirs("photos", exist_ok=True)
+                        shutil.copy(os.path.join(self.image_path, filename), destination)
 
-        for i in range(15):
-            data[f"Reason {i + 1}"] = self.reason_entries[i].get()
-            data[f"Description {i + 1}"] = self.description_entries[i].get()
+                        reason = self.reason_entries[0].get()
+                        description = self.description_entries[0].get()
 
-        df = pd.DataFrame(data)
+                        if (reason, description) not in self.reason_description_map:
+                            self.reason_description_map[(reason, description)] = [image_name]
+                        else:
+                            self.reason_description_map[(reason, description)].append(image_name)
 
-        excel_filename = "validation_data.xlsx"
+                        # Saving details to Excel
+                        data = {
+                            "Image Path": [image_name],
+                            "Validation": ["Valid" if valid_value == 1 else "Invalid"],  # Include the 'Valid'/'Invalid' value,
+                            "Reason": [reason],
+                            "Description": [description]
+                        }
+                        df = pd.DataFrame(data)
 
-        if os.path.exists(f'{current_dir}/{excel_filename}'):
-            existing_data = pd.read_excel(excel_filename)
+                        excel_filename = "validation_data.xlsx"
 
-            existing_image_names = existing_data["Image Path"].tolist()
-            if image_name in existing_image_names:
-                messagebox.showinfo("Information", f"Image '{image_name}' already exists in the data.")
-                return
+                        if os.path.exists(excel_filename):
+                            existing_data = pd.read_excel(excel_filename)
+                            existing_data = pd.concat([existing_data, df], ignore_index=True)
+                            existing_data.to_excel(excel_filename, index=False)
+                        else:
+                            df.to_excel(excel_filename, index=False)
 
-            existing_data = pd.concat([existing_data, df], ignore_index=True)
-            existing_data.to_excel(f'{current_dir}/{excel_filename}', index=False)
+                        processed_images.append(image_name)
+                    else:
+                        messagebox.showinfo("Info", f"Image '{image_name}' already exists in the data.")
+
+            if processed_images:
+                processed_str = ", ".join(processed_images)
+                messagebox.showinfo("Success", f"Images {processed_str} processed and copied to 'photos' folder.")
         else:
-            df.to_excel(f'{current_dir}/{excel_filename}', index=False)
-            # Copy the image to the 'photos' directory
-        shutil.copy(self.image_path, destination)
-        messagebox.showinfo("Success", f"Data saved to {excel_filename} and image copied to 'photos' folder.")
+            # Existing logic for single image processing remains unchanged
+            image_name = os.path.basename(self.image_path)
+            if not self.image_not_in_excel(image_name):
+                messagebox.showinfo("Info", f"Image '{image_name}' already exists in the data.")
+                return # Exit the function
+            destination = os.path.join("photos", image_name)
+            os.makedirs("photos", exist_ok=True)
+            Image.open(self.image_path).save(destination)
 
-    def on_close(self):
+            reason = self.reason_entries[0].get()
+            description = self.description_entries[0].get()
+
+            if (reason, description) not in self.reason_description_map:
+                self.reason_description_map[(reason, description)] = [image_name]
+            else:
+                self.reason_description_map[(reason, description)].append(image_name)
+
+            # Saving details to Excel
+            data = {
+                "Image Path": [image_name],
+                "Validation": ["Valid" if valid_value == 1 else "Invalid"],  # Include the 'Valid'/'Invalid' value,
+                "Reason": [reason],
+                "Description": [description]
+            }
+            df = pd.DataFrame(data)
+
+            excel_filename = "validation_data.xlsx"
+
+            if os.path.exists(excel_filename):
+                existing_data = pd.read_excel(excel_filename)
+                existing_data = pd.concat([existing_data, df], ignore_index=True)
+                existing_data.to_excel(excel_filename, index=False)
+            else:
+                df.to_excel(excel_filename, index=False)
+
+            messagebox.showinfo("Success", "Data saved and image copied to 'photos' folder.")
+
+        self.clear_entries_except_line_numbers()
+
+    def image_not_in_excel(self, image_name):
+        """
+        Checks if the image is already present in the Excel file
+        :param image_name:
+        :return:
+        """
         excel_filename = "validation_data.xlsx"
         if os.path.exists(excel_filename):
-            backup_dir = f'{current_dir}/"backups'
+            existing_data = pd.read_excel(excel_filename)
+            return image_name not in existing_data["Image Path"].values
+        return True
+
+    def on_close(self):
+        """
+        Handles the closing of the application
+        :return:
+        """
+        excel_filename = "validation_data.xlsx"
+        if os.path.exists(excel_filename):
+            backup_dir = "backups"
             os.makedirs(backup_dir, exist_ok=True)
             timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             backup_filename = os.path.join(backup_dir, f"backup_validation_data_{timestamp}.xlsx")
@@ -120,10 +232,28 @@ class ImageValidator:
         self.root.destroy()
 
     def clear_entries_except_line_numbers(self):
+        """
+        Clears the reason and description entries
+        :return:
+        """
         for entry in self.reason_entries:
             entry.delete(0, tk.END)
         for entry in self.description_entries:
             entry.delete(0, tk.END)
+        # Clear the displayed image
+        self.clear_image()
+        self.image_path = ""
+
+    def clear_image(self):
+        """
+        Clears the image displayed on the GUI
+        :return:
+        """
+        img_labels = self.root.grid_slaves(row=0, column=3)
+        if img_labels:
+            img_label = img_labels[0]
+            img_label.grid_forget()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
