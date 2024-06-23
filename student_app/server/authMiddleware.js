5bfbd834502e068f7147eda5eb5089f9ad9d1a58
Yair_Davidoff <88335811+yairda2@users.noreply.github.com>
2024-06-24 00:56:39 +0300

    update, and save before orgenaize

--- student_app/server/authMiddleware.js
+++ student_app/server/authMiddleware.js
@@ -1,20 +1,25 @@
-// authMiddleware.js
-
 const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'your_secret_jwt_key';
 
-function checkToken(req, res, next) {
+function verifyToken(req, res, next) {
     const token = req.cookies.token;
     if (!token) {
-        return res.status(401).json({ message: 'NoToken' });
+        if (req.accepts('html')) {
+            return res.redirect(`/login?error=NoToken`);
+        }
+        return res.status(401).json({ message: 'No token provided' });
     }
 
-    jwt.verify(token, 'your_secret_jwt_key', (err, decoded) => {
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
         if (err) {
-            return res.status(401).json({ message: 'InvalidToken' });
+            if (req.accepts('html')) {
+                return res.redirect(`/login?error=InvalidToken`);
+            }
+            return res.status(401).json({ message: 'Invalid token' });
         }
         req.user = decoded;
         next();
     });
 }
 
-module.exports = checkToken;
+module.exports = verifyToken;
